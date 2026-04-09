07970e43274032bc30dbe761d1b4322950c36be0
George Butler <george.butler@bytedance.com>
2026-04-09 13:05:31 -0700

    feat(AiPage): improve responsive design and add chat UI
    - Replace static placeholder with a responsive chat message and textarea input
    - Add responsive padding, margin, and text sizing for mobile to desktop views
    - Update service worker cache revision for index.html

--- dev-dist/sw.js
+++ dev-dist/sw.js
@@ -85,7 +85,7 @@ define(['./workbox-785e9e58'], (function (workbox) { 'use strict';
     "revision": "3ca0b8505b4bec776b69afdba2768812"
   }, {
     "url": "index.html",
-    "revision": "0.5s5vngjtjos"
+    "revision": "0.eb199s7mkho"
   }], {});
   workbox.cleanupOutdatedCaches();
   workbox.registerRoute(new workbox.NavigationRoute(workbox.createHandlerBoundToURL("index.html"), {
