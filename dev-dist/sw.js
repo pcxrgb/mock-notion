c2ea3fe4bf15a505ea0886fb18ff36d291be579d
George Butler <george.butler@bytedance.com>
2026-04-09 13:18:57 -0700

    style: improve responsive design for mobile and tablet screens
    - Update padding, font sizes, and spacing in TodoPage for better mobile experience
    - Simplify AiPage subtitle text from "Here are a few things I can do, or ask me anything!" to "Ask me anything!"
    - Adjust checkbox sizes and text wrapping for improved mobile usability
    - Service worker cache revision updated due to build changes

--- dev-dist/sw.js
+++ dev-dist/sw.js
@@ -85,7 +85,7 @@ define(['./workbox-785e9e58'], (function (workbox) { 'use strict';
     "revision": "3ca0b8505b4bec776b69afdba2768812"
   }, {
     "url": "index.html",
-    "revision": "0.eb199s7mkho"
+    "revision": "0.a98bndgi4l4"
   }], {});
   workbox.cleanupOutdatedCaches();
   workbox.registerRoute(new workbox.NavigationRoute(workbox.createHandlerBoundToURL("index.html"), {
